a6eca8481717291dd805b8ae3b208364f0106e3b
Caçula10 <flaviodesouzamarques@gmail.com>
2020-07-21 00:08:26 +0100

    Upload

--- Main.py
+++ Main.py
@@ -1,16 +1,20 @@
 from Database.Database.Database import cursor, conn
 from Scraping.BeautifulSoup.Beautifulsoup import *
 
+first = str(main_container_link[0]).split()
+for i in first:
+    if 'href' in i:
+        Link = i
+        break
+
 dicionario = dict()
-dicionario = {'Name': str(main_container_note[0].a.span.text).strip(),
+dicionario = {'Link': Link.strip(),
+              'Name': str(main_container_note[0].a.span.text).strip(),
               'Price': str(main_container_preco[0].span.text).strip(),
               'Stars': str(main_container_star[0].text).strip(),
-              'Reviews': str(main_container_reviews[0].text).strip()}
+              'Reviews': str(main_container_reviews[0].text).strip()
+              }
 
-cursor.execute("insert into [dbo].[PRODUTOS]"
-               "values (?,?,?,?)",
-               (dicionario['Name'],
-                dicionario['Price'],
-                dicionario['Stars'],
-                dicionario['Reviews']))
+cursor.execute("insert into [dbo].[PRODUTOS] values (?,?,?,?,?)",
+               (dicionario['Link'], dicionario['Name'], dicionario['Price'], dicionario['Stars'], dicionario['Reviews']))
 conn.commit()
--- Scraping/BeautifulSoup/Beautifulsoup.py
+++ Scraping/BeautifulSoup/Beautifulsoup.py
@@ -10,3 +10,4 @@ main_container_note = html_soup.find_all('div', class_='search-result-product-ti
 main_container_preco = html_soup.find_all('span', class_='price display-inline-block arrange-fit price price-main')
 main_container_star = html_soup.find_all('span', class_='visuallyhidden seo-avg-rating')
 main_container_reviews = html_soup.find_all('span', class_='seo-review-count visuallyhidden')
+main_container_link = html_soup.find_all('a', class_='product-title-link line-clamp line-clamp-2 truncate-title')
