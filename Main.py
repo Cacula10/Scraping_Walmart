226e7c2c553f1b3600f8dcae7d320e7b8139ebce
Caçula10 <flaviodesouzamarques@gmail.com>
2020-10-09 15:50:57 +0100

    Create structure type list of all regs in Website

--- Main.py
+++ Main.py
@@ -1,46 +1,37 @@
-from Database.Database.Database import cursor, conn
-from Scraping.BeautifulSoup.Beautifulsoup import *
-from datetime import datetime
+from Scraping.BeautifulSoup.Beautifulsoup import * # rodando a classe beautifulsoup
 from Def.Defs.Colors import *
 
-from pygame import mixer
-mixer.init()
-mixer.music.load('mortal.mp3')
-mixer.music.play()
-parar = input('Digite ENTER para interromper...')
+#from pygame import mixer
+#mixer.init()
+#mixer.music.load('mortal.mp3')
+#mixer.music.play()
+#parar = input('Digite ENTER para interromper...')
 
+lista = [[], [], [], []]
+prov = []
 
-first = str(main_container_link[0]).split()
-for i in first:
-    if 'href' in i:
-        link = i
-        break
+for i in main_container_note:
+    prov.append(i.a.span.text)
+    lista[0].append(prov[:])
+    prov.clear()
 
-dicionario = {'Link': str(link.strip()),
-              'Date': str(str(datetime.now())),
-              'Name': str(main_container_note[0].a.span.text).strip(),
-              'Price': str(main_container_preco[0].span.text).strip(),
-              'Stars': str(main_container_star[0].text).strip(),
-              'Reviews': str(main_container_reviews[0].text).strip()}
+for i in main_container_preco:
+    prov.append(i.span.text)
+    lista[1].append(prov[:])
+    prov.clear()
 
-cursor.execute("select * from produtos where link = (?)", dicionario["Link"])
-all_reg = cursor.fetchall()
+for i in main_container_star:
+    prov.append(i.text)
+    lista[2].append(prov[:])
+    prov.clear()
 
-if len(all_reg) == 0 or all_reg[-1][3] != dicionario["Price"]:
-    cursor = conn.cursor()
-    cursor.execute("insert into [dbo].[PRODUTOS] values (?,?,?,?,?,?)",
-                   (str(dicionario['Link']),
-                    str(dicionario['Date']),
-                    str(dicionario['Name']),
-                    str(dicionario['Price']),
-                    str(dicionario['Stars']),
-                    str(dicionario['Reviews'])))
-    conn.commit()
-    conn.close()
-    def_cores('<<< REGISTRO CADASTRADO OU ATUALIZADO NO BANCO >>>', 'azul')
-    def_cores('<<< DESCONECTADO SESSÃO DA BASE >>>', 'azul')
-else:
-    conn.close()
-    def_cores('<<< NÃO VOU ADICIONAR, POIS JA ESTÁ CADASTRADO NO BANCO E TAMBÉM NÃO TIVEMOS ALTERAÇÃO NO PREÇO >>>', 'vermelho')
-    def_cores('<<<DESCONECTADO SESSÃO DA BASE >>>', 'azul')
+for i in main_container_reviews:
+    prov.append(i.text)
+    lista[3].append(prov[:])
+    prov.clear()
+
+# utilizado o metodo zip para organizar lista
+aninhado = []
+for i in tuple(zip(lista[0], lista[1], lista[2], lista[3])):
+    aninhado.append(i)
 
